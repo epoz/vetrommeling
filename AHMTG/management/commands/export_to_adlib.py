9c37a97ba3db1ea2eaa9753a803d2568fc3083b2
Etienne Posthumus <eposthumus@gmail.com>
2012-11-13 17:14:39 +0100

    Command to do AdLib exports, to be called from cronjob periodically

---
+++ AHMTG/management/commands/export_to_adlib.py
@@ -0,0 +1,12 @@
+# Export tags to a Adlib Database
+
+from django.core.management.base import BaseCommand
+from AHMTG import util
+    
+class Command(BaseCommand):
+    help = "Write all un-exported answers to the AdLib database"
+    
+    def handle(self, *args, **options):
+        tag_map = util.tag_export()
+        util.write_tags_to_template(tag_map)
+        print 'Wrote %s tags' % len(tag_map)
