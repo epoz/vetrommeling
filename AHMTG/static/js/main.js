a4284a7916c48a24238415efc32aaf98d6951009
Etienne Posthumus <eposthumus@gmail.com>
2012-11-13 17:03:19 +0100

    Start of PyBossa support. If empty antwoord, submit it.

--- AHMTG/static/js/main.js
+++ AHMTG/static/js/main.js
@@ -14,15 +14,26 @@
     });
   });
 
+  $('#makeseriespybossa').click(function(event) {
+    var loc, v, _i, _len, _ref;
+    event.preventDefault();
+    loc = 'naam=' + encodeURI($('#serienaam').val());
+    loc += '&adlibsearch=' + encodeURI($('#serieadlibsearch').val());
+    _ref = $('.vraag:checked');
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      v = _ref[_i];
+      loc += '&vraag=' + $(v).attr('rel');
+    }
+    return $.post('/makepybossa/', loc, function(data) {
+      return alert(data);
+    });
+  });
+
   $('#volgende').click(function(event) {
     var antw;
     event.preventDefault();
     antw = $('input[name="optie"]:checked').val();
     if (!antw) antw = $('#antwoord').val();
-    if (!antw) {
-      document.location = '.';
-      return;
-    }
     return $.post('/answer/', {
       'serievraag_pk': SERIEVRAAG_PK,
       'obj_id': OBJ_ID,
